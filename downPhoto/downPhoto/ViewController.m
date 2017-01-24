be7693dd0723c96a68661deb6e600d8078218853
zhaok <zhaok@zhaokaideMacBook-Pro.local>
2017-01-24 15:26:28 +0800

    add img

--- downPhoto/downPhoto/ViewController.m
+++ downPhoto/downPhoto/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+@interface ViewController ()<UIScrollViewDelegate,UITableViewDelegate,UITableViewDataSource> {
+    CGFloat imgH;
+}
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIImageView *img;
 
 @end
 
@@ -16,14 +22,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    imgH = [[UIScreen mainScreen] bounds].size.width/1.6;
+    
+    [self.view addSubview:self.tableView];
 }
 
+- (UITableView *)tableView {
+    _tableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.contentInset = UIEdgeInsetsMake(imgH, 0, 0, 0);
+    
+    _img = [[UIImageView alloc] initWithFrame:CGRectMake(0, -imgH, [[UIScreen mainScreen] bounds].size.width, imgH)];
+    [_img setImage:[UIImage imageNamed:@"test"]];
+    _img.contentMode = UIViewContentModeScaleAspectFill;
+    [_tableView addSubview:_img];
+    
+    return _tableView;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 30;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellID = @"UITableViewCell";
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    cell.textLabel.text =  [NSString stringWithFormat:@"cell-%ld",indexPath.row];
+    return cell;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    CGPoint point = scrollView.contentOffset;
+    NSLog(@"pointX=%f,pointY=%f",point.x,point.y);
+    if (point.y < -imgH) {
+        CGRect rect = _img.frame;
+        rect.origin.y = point.y;
+        rect.size.height = -point.y;
+        _img.frame = rect;
+    }
+}
 
 @end
