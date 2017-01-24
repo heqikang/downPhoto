b3fa2b1a70ac28062b1ba5ea270042225a036b3c
zhaok <zhaok@zhaokaideMacBook-Pro.local>
2017-01-24 11:24:50 +0800

    new project

---
+++ downPhoto/downPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  downPhoto
+//
+//  Created by 赵凯 on 2017/1/24.
+//  Copyright © 2017年 赵凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ downPhoto/downPhoto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  downPhoto
+//
+//  Created by 赵凯 on 2017/1/24.
+//  Copyright © 2017年 赵凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
